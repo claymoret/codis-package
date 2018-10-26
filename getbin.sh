b2d75bdbb47e5749488fa722debadef3aa8a8319
claymoret <claymoret@163.com>
2018-10-26 11:16:41 +0800

    add info

--- getbin.sh
+++ getbin.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#wget https://github.com/CodisLabs/codis/archive/3.2.2.tar.gz
+
 wget https://github.com/CodisLabs/codis/releases/download/3.2.2/codis3.2.2-go1.9.2-linux.tar.gz
 tar zxvf codis3.2.2-go1.9.2-linux.tar.gz
 mv codis3.2.2-go1.9.2-linux bin 
