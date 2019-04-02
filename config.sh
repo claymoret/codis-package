338c76d51106564e6c03ba084cf054ce8f028a0a
claymoret <claymoret@163.com>
2019-04-03 01:47:08 +0800

    copy server

--- config.sh
+++ config.sh
@@ -2,14 +2,14 @@
 
 
 ### admin/codis-fe-admin.sh
-COORDINATOR_ADDR="10.70.1.101,10.70.1.102,10.70.1.103,10.70.1.104,10.70.1.105"
+COORDINATOR_ADDR="10.70.1.122,10.70.1.123,10.70.1.124,10.70.1.125,10.70.1.126"
 CODIS_FE_ADDR="0.0.0.0:8080"
 
 sed -i "s/^COORDINATOR_ADDR.*$/COORDINATOR_ADDR=\"$COORDINATOR_ADDR\"/" admin/codis-fe-admin.sh
 sed -i "s/^CODIS_FE_ADDR.*$/CODIS_FE_ADDR=\"$CODIS_FE_ADDR\"/" admin/codis-fe-admin.sh
 
 ### admin/codis-proxy-admin.sh
-CODIS_DASHBOARD_ADDR="10.70.1.102:18080"
+CODIS_DASHBOARD_ADDR="10.70.1.123:18080"
 
 sed -i "s/^CODIS_DASHBOARD_ADDR.*$/CODIS_DASHBOARD_ADDR=\"$CODIS_DASHBOARD_ADDR\"/" admin/codis-proxy-admin.sh
 
@@ -19,7 +19,7 @@ redis_port=7900
 sed -i "s/6379/$redis_port/g" admin/codis-server-admin.sh
 
 ### config/dashboard.toml
-coordinator_addr="10.70.1.101,10.70.1.102,10.70.1.103,10.70.1.104,10.70.1.105"
+coordinator_addr="10.70.1.122,10.70.1.123,10.70.1.124,10.70.1.125,10.70.1.126"
 product_name="biugo-codis"
 admin_addr="0.0.0.0:18080"
 
@@ -30,7 +30,7 @@ sed -i "s/^admin_addr.*$/admin_addr = \"$admin_addr\"/" config/dashboard.toml
 ### config/proxy.toml
 admin_addr="0.0.0.0:11080"
 proxy_addr="0.0.0.0:19000"
-jodis_addr="10.70.1.101,10.70.1.102,10.70.1.103,10.70.1.104,10.70.1.105"
+jodis_addr="10.70.1.122,10.70.1.123,10.70.1.124,10.70.1.125,10.70.1.126"
 
 sed -i "s/^product_name.*$/product_name = \"$product_name\"/" config/proxy.toml
 sed -i "s/^admin_addr.*$/admin_addr = \"$admin_addr\"/" config/proxy.toml
@@ -44,7 +44,7 @@ sed -i "s/6379/$redis_port/g" config/redis.conf
 ### config/sentinel.conf
 sentinel_port=27900
 sentinel_master_prefix=biugo-codis-
-sentinel_master_list="10.70.1.102 10.70.1.104"
+sentinel_master_list="10.70.1.123 10.70.1.125"
 
 sed -i "s/26379/$sentinel_port/g" config/sentinel.conf
 sed -i "/^sentinel monitor.*$/d" config/sentinel.conf
